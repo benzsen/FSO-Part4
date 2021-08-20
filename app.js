a7df34f11ac9ba841b2d8021d2129c61f5a2f3bf
Benny Luo <bennyluo95@gmail.com>
2021-08-19 21:29:56 -0400

    Updated for part 5.18 (Cypress)

--- app.js
+++ app.js
@@ -24,11 +24,15 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.reqLogger)
 app.use(middleware.tokenExtractor)
-//app.use(middleware.userExtractor)
 
 app.use('/', blogRouter)
 app.use("/api/users", userRouter)
 app.use("/api/login", loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
