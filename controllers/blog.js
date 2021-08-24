ab926dbca812adcd0d91d90d9ad6bd497819d3b8
Benny Luo <bennyluo95@gmail.com>
2021-08-23 22:56:59 -0400

    Updated blogServices post to update()

--- controllers/blog.js
+++ controllers/blog.js
@@ -103,10 +103,8 @@ const user = await User.findById(req.user.id)
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
 
-  await user.save()
+  await user.update()
   res.status(201).json(savedBlog)
-
-
 })
 
 module.exports = blogRouter
